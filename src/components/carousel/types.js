9f3a288f9feaf5b71b2607bb2d4c16a44954e14a
Diego Mello <diegolmello@gmail.com>
2020-11-24 13:51:44 -0300

    build

---
+++ src/components/carousel/types.js
@@ -0,0 +1,6 @@
+export let PageControlPosition;
+
+(function (PageControlPosition) {
+  PageControlPosition["OVER"] = "over";
+  PageControlPosition["UNDER"] = "under";
+})(PageControlPosition || (PageControlPosition = {}));
